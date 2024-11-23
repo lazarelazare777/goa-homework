8d19c1a1a5ec812f97e1805d18e145ddd3a9e5eb
lazarelazare777 <lazaregogoberidze56@gmail.com>
2024-11-23 14:35:46 +0400

    first commit

---
+++ day 0/classwork/day0.py
@@ -0,0 +1,24 @@
+#print("lazare")
+#name = "lazare"
+# string (str)-სტრინგი არის "ბრჭყალებში" მოქცეული სიტყვა
+# integer (int)-ციფრები 
+# float - ათწილადები
+# boolean (bool)-true და false
+# print("2+2")
+# prinprint(2+2)
+# t(3>=3)
+# name = input("enter your name ")
+# print("hello " + name)     
+# print(float(3))
+      
+age1=int(input("enter your 1 age "))
+age2=int(input("enter your 2 age "))
+age3=int(input("enter your 3 age "))
+age4=int(input("enter your 4 age "))
+age5=int(input("enter your 5 age "))
+print(age1 + 25)
+print(age2 + 25)
+print(age3 + 25)
+print(age4 + 25)
+print(age5 + 25)
+
---
+++ day 0/homework/homework.py
@@ -0,0 +1,9 @@
+# შექმენი 5 ცვლადი:
+# სახელი, გვარი, ასაკი, სიმაღლე, წონა. შემდეგ კი ააწყვე წინადადება:
+# გამარჯობა, მე ვარ {}, მე ვარ {} წლის, ჩემი სიმაღლეა {} მეტრი და ჩემი წონაა {} კგ
+name = "lazare "
+lastname = "gogoberidze "
+age = 12 
+height = 1.69
+weight = 48
+print("hay , i'm " + name + lastname , "i'm " + str(age) + " years old, my heigth is " +str(height)  + "sm , my weight is " + str(weight) + "kg " ) 
